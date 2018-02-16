be557135e2e6c7288cc5ee8d36b0fad33f0b8009
Krishna Dwypayan <32203989+krishnadwypayan@users.noreply.github.com>
2018-02-16 16:52:40 +0530

    Add files via upload

---
+++ TweetFilter/filterTweets.py
@@ -0,0 +1,156 @@
+from main import *
+from pymongo import Connection
+from pymongo.errors import ConnectionFailure
+import pymongo
+
+# Filter the tweets according to the given expression evaluation.
+def conditionFilter(mongo, expression, page):
+	conditions = ['<', '>', '<=', '>=', '=', '!=']
+	condition = ""
+	pos = 0
+	for c in conditions:
+		pos = expression.find(c)
+		if pos is not -1:
+			condition = c
+			break
+	column_name = expression[0:pos]
+	value = expression[pos+len(condition):]
+	if condition == "=":
+		condition = "=="
+	tweets = mongo.db.tweets
+	output = []
+	column = '%s' %column_name
+	for t in tweets.find():
+		exp = [str(t[column]), condition, value]
+		if eval(" ".join(exp)) == True:
+			output_dict = dict()
+			output_dict['tweet_time'] = t['tweet_time']
+			output_dict['tweet'] = t['tweet_text']
+			output_dict['screen_name'] = t['tweet_screen_name']
+			output_dict[column_name] = t[column]
+			output.append(output_dict)
+
+	outtweets = [[tweet['tweet_time'], tweet['tweet'].encode("utf-8"), tweet['screen_name'].encode("utf-8"), tweet[column]] for tweet in output[(page-1)*10:(page-1)*10 + 10]]	
+	with open(os.path.join(os.getcwd()+'/CSV/%s_filtered_tweets_%s.csv' %(column, str(page))), 'wb') as f:
+		writer = csv.writer(f)
+		writer.writerow(["created_at", "text", "screen_name", column_name])
+		writer.writerows(outtweets)
+	pass
+	
+	return output[(page-1)*10:(page-1)*10 + 10]
+
+# Sort by tweets according to the date(ascending/descending)
+def sortByDate(mongo, asc, page):
+	tweets = mongo.db.tweets
+	output = []
+	for t in tweets.find().sort('tweet_time'):
+		output.append({
+				'tweet_time' : t['tweet_time'],
+				'tweet' : t['tweet_text'],
+				'screen_name' : t['tweet_screen_name']
+			})
+
+	desc_output = []
+	if asc.lower() == "descending":
+		for v in reversed(output):
+			desc_output.append(v)
+		output = desc_output
+
+	outtweets = [[tweet['tweet_time'], tweet['tweet'].encode("utf-8"), tweet['screen_name'].encode("utf-8")] for tweet in output[(page-1)*10:(page-1)*10 + 10]]	
+	with open(os.path.join(os.getcwd()+'/CSV/%s_order_sorted_tweets_%s.csv' %(asc, str(page))), 'wb') as f:
+		writer = csv.writer(f)
+		writer.writerow(["created_at", "text", "screen_name"])
+		writer.writerows(outtweets)
+	pass
+
+	return output[(page-1)*10:(page-1)*10 + 10]
+
+# Filter tweets to generate output containing only text, tweet_time and screen_name.
+def getTweetsWithText(mongo, page):
+	tweets = mongo.db.tweets
+	output = []
+	try:
+		c = Connection(host="localhost", port=27017)
+	except ConnectionFailure, e:
+		sys.stderr.write("Could not connect to MongoDB: %s" % e)
+		sys.exit(1)
+	dbh = c["tweets_db"]
+	assert dbh.connection == c
+	tweets_get = tweets.find({}).sort("tweet_time", pymongo.DESCENDING)
+	for t in tweets_get:
+		output.append({
+				'tweet_time' : t['tweet_time'],
+				'tweet' : t['tweet_text'],
+				'screen_name' : t['tweet_screen_name']
+				})
+
+	outtweets = [[tweet['tweet_time'], tweet['tweet'].encode("utf-8"), tweet['screen_name'].encode("utf-8")] for tweet in output[(page-1)*10:(page-1)*10 + 10]]
+	with open(os.path.join(os.getcwd()+'/CSV/get_tweets_with_text_only_%s.csv' % str(page)), 'wb') as f:
+		writer = csv.writer(f)
+		writer.writerow(["created_at", "text", "screen_name"])
+		writer.writerows(outtweets)
+	pass
+	return output[(page-1)*10:(page-1)*10 + 10]
+
+# Search for the specific word appearing anywhere in the tweet details.
+def regexMatchTweets(mongo, keyword, page):
+	tweets = mongo.db.tweets
+	output = []
+	for t in tweets.find().sort("tweet_time", pymongo.DESCENDING):
+		if re.search(keyword, str(t)):
+			output_dict = dict()
+			output_dict['tweet_time'] = t['tweet_time']
+			output_dict['tweet_text'] = t['tweet_text']
+			output_dict['tweet_screen_name'] = t['tweet_screen_name']
+			output.append(output_dict)
+	
+	outtweets = [[tweet['tweet_time'], tweet['tweet_text'].encode("utf-8"), tweet['tweet_screen_name'].encode("utf-8")] for tweet in output[(page-1)*10:(page-1)*10 + 10]]
+	with open(os.path.join(os.getcwd()+'/CSV/%s_search_tweets_%s.csv' %(keyword, str(page))), 'wb') as f:
+		writer = csv.writer(f)
+		writer.writerow(["created_at", "text", "screen_name"])
+		writer.writerows(outtweets)
+	pass
+
+	return output[(page-1)*10:(page-1)*10 + 10]
+
+# Search for the text in the screen_name or the tweet_text.
+def textSearchInTweetOrUsername(mongo, keyword, page):
+	tweets = mongo.db.tweets
+	output = []
+	for t in tweets.find().sort("tweet_time", pymongo.DESCENDING):
+		text = t['tweet_text'].lower().encode("utf-8").split()
+		screen_name = t['tweet_screen_name'].lower().encode("utf-8").split()
+		if keyword in text or keyword in screen_name:
+			output.append({
+					'tweet_time' : t['tweet_time'],
+					'tweet' : t['tweet_text'],
+					'screen_name' : t['tweet_screen_name']
+					})
+
+	outtweets = [[tweet['tweet_time'], tweet['tweet'].encode("utf-8"), tweet['screen_name'].encode("utf-8")] for tweet in output[(page-1)*10:(page-1)*10 + 10]]
+	with open(os.path.join(os.getcwd()+'/CSV/%s_matched_tweets_%s.csv' % (keyword, str(page))), 'wb') as f:
+		writer = csv.writer(f)
+		writer.writerow(["created_at", "text", "screen_name"])
+		writer.writerows(outtweets)
+	pass
+	return output[(page-1)*10:(page-1)*10 + 10]
+
+# Search for the tweets with url mentions and return them.
+def filterTweetsByURLs(mongo, page):
+	tweets = mongo.db.tweets
+	output = []
+	for t in tweets.find().sort("tweet_time", pymongo.DESCENDING):
+		if t['tweet_entities_urls'] is not "":
+			output.append({
+					'tweet_time' : t['tweet_time'],
+					'tweet' : t['tweet_text'],
+					'screen_name' : t['tweet_screen_name'],
+					'url_mentions' : t['tweet_entities_urls']
+				})
+	outtweets = [[tweet['tweet_time'], tweet['tweet'].encode("utf-8"), tweet['screen_name'].encode("utf-8"), tweet['url_mentions']] for tweet in output[(page-1)*10:(page-1)*10 + 10]]
+	with open(os.path.join(os.getcwd()+'/CSV/url_mentions_tweets_%s.csv' % str(page)), 'wb') as f:
+		writer = csv.writer(f)
+		writer.writerow(["created_at", "text", "screen_name", "url_mentions"])
+		writer.writerows(outtweets)
+	pass
+	return output[(page-1)*10:(page-1)*10 + 10]	
---
+++ TweetFilter/locations.py
@@ -0,0 +1,86 @@
+from googleplaces import GooglePlaces, types, lang
+import re
+import time
+from main import mongo as mongo
+import countries
+import click
+
+# Google API Key
+YOUR_API_KEY = 'AIzaSyBAaSHwdm7HgjEx855RGM6vszvWVm99t_o'
+google_places = GooglePlaces(YOUR_API_KEY)
+
+# Clean the html content from a string
+def cleanhtml(raw_html):
+	cleanr = re.compile('<.*?>')
+	cleantext = re.sub(cleanr, '', raw_html)
+	return cleantext
+
+# Return the SEARCH_INDEX where each SEARCH_INDEX holds specific typeS.
+def getSearchIndex(val):
+	SEARCH_INDEX1 = [types.TYPE_MUSEUM, types.TYPE_AQUARIUM, types.TYPE_ZOO, types.TYPE_SYNAGOGUE]
+	SEARCH_INDEX2 = [ types.TYPE_PARKING, types.TYPE_PARK, types.TYPE_MEAL_DELIVERY, types.TYPE_STADIUM]
+	SEARCH_INDEX3 = [types.TYPE_MOSQUE, types.TYPE_HINDU_TEMPLE, types.TYPE_CHURCH]
+	SEARCH_INDEX4 = [types.TYPE_CAFE, types.TYPE_BAR, types.TYPE_CASINO , types.TYPE_SHOPPING_MALL]
+	SEARCH_INDEX5 = [types.TYPE_CITY_HALL, types.TYPE_LIBRARY, types.TYPE_LOCAL_GOVERNMENT_OFFICE]
+	SEARCH_INDEX6 = [types.TYPE_TRAIN_STATION, types.TYPE_BUS_STATION]
+	SEARCH_INDEX7 = [types.TYPE_HOSPITAL, types.TYPE_ATM, types.TYPE_PET_STORE]
+	SEARCH_INDEX8 = [types.TYPE_POLICE]
+	SEARCH_INDEX9 = [types.TYPE_ART_GALLERY, types.TYPE_MOVIE_THEATER, types.TYPE_BOOK_STORE]
+	SEARCH_INDEX10 = [types.TYPE_DENTIST, types.TYPE_BEAUTY_SALON, types.TYPE_GAS_STATION]
+	SEARCH_INDEX11 = [types.TYPE_BANK, types.TYPE_BAKERY, types.TYPE_ELECTRONICS_STORE]
+	SEARCH_INDEX12 = [types.TYPE_SCHOOL, types.TYPE_STORE]
+	SEARCH_INDEX13 =[types.TYPE_POST_OFFICE, types.TYPE_TAXI_STAND]
+
+	mapper = {1: SEARCH_INDEX1, 2: SEARCH_INDEX2, 3: SEARCH_INDEX3, 4: SEARCH_INDEX4, 
+	5: SEARCH_INDEX5, 6: SEARCH_INDEX6, 7: SEARCH_INDEX7, 8: SEARCH_INDEX8, 9: SEARCH_INDEX9, 
+	10: SEARCH_INDEX10, 11: SEARCH_INDEX11, 12: SEARCH_INDEX12, 13: SEARCH_INDEX13,}
+
+	return mapper[val]
+
+# Returns a set containing the locations populated nearby the given search location
+def populateNearbyLocations(location):
+	output = set()
+	for i in range(1, 14):
+		print i, "/14 : "
+		with click.progressbar(range(1000000)) as bar:
+			for b in bar:
+				pass 
+			query_result = google_places.nearby_search(location=str(location), radius=500, types=getSearchIndex(i))
+			for place in query_result.places:
+				place.get_details()
+				for p in cleanhtml(place.details['adr_address']).split(','):
+					output.add(p)
+				time.sleep(1.1)
+	return output
+
+# Returns the tweets in the radius of 500m of the specified location
+def getTweets(place, page):
+	locations = populateNearbyLocations(place)
+	updated_locations = locations.copy()
+	for locs in locations:
+		for country in countries.countries:
+			if country['name'] == locs.encode("utf-8").strip():
+				print locs.encode("utf-8")
+				updated_locations.remove(locs)
+	tweets = mongo.db.tweets
+	output = []
+	for t in tweets:
+		for loc in updated_locations:
+			if re.search(loc, str(t['tweet_user_location'])):
+				tweet_details = {'tweet_id' : tweet_id, 'tweet_time' : tweet_time, 'tweet_screen_name' : tweet_screen_name,
+							'tweet_text' : tweet_text, 'tweet_lang' : tweet_lang, 'tweet_user_followers_count' : tweet_user_followers_count,
+							'tweet_user_friends_count' : tweet_user_friends_count, 'tweet_user_listed_count' : tweet_user_listed_count,
+							'tweet_user_favourites_count' : tweet_user_favourites_count, 'tweet_retweet_count' : tweet_retweet_count,
+							'tweet_in_reply_to_screen_name' : tweet_in_reply_to_screen_name, 'tweet_place_country' : tweet_place_country,
+							'tweet_place_name' : tweet_place_name, 'tweet_user_location' : tweet_user_location,	'tweet_entities_hashtags' : tweet_entities_hashtags, 
+							'tweet_entities_urls' : tweet_entities_urls, 'tweet_entities_user_mentions_name' : tweet_entities_user_mentions_name, 
+							'tweet_entities_user_mentions_screen_name' : tweet_entities_user_mentions_screen_name}
+				output.append(tweet_details)
+
+	outtweets = [[tweet['tweet_time'], tweet['tweet'].encode("utf-8"), tweet['screen_name'].encode("utf-8"), tweet['tweet_user_location']] for tweet in output[(page-1)*10:(page-1)*10 + 10]]
+	with open(os.path.join(os.getcwd()+'/CSV/in_%s_radius_tweets_%s.csv' % (place, str(page))), 'wb') as f:
+		writer = csv.writer(f)
+		writer.writerow(["created_at", "text", "screen_name", "location"])
+		writer.writerows(outtweets)
+	pass
+	return output[(page-1)*10:(page-1)*10 + 10]
